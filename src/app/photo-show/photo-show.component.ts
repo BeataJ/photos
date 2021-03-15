306e2d2b79f0b53c601a37c6b50d41756d67b217
beataj <beata@emc2wave.com>
2021-03-14 17:44:31 -0700

    recreated project

---
+++ src/app/photo-show/photo-show.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../photos.service';
+
+@Component({
+  selector: 'app-photo-show',
+  templateUrl: './photo-show.component.html',
+  styleUrls: ['./photo-show.component.css']
+})
+export class PhotoShowComponent implements OnInit {
+  photoUrl: string;
+
+  constructor(private pothoService: PhotosService) { 
+    this.fetchPhoto();
+  }
+
+  onClick() {
+    this.fetchPhoto();
+  }
+
+  fetchPhoto() {
+    this.pothoService.getPhoto().subscribe(response => {
+      this.photoUrl = response.urls.regular;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
